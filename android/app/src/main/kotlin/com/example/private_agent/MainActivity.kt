332468b1601aa14119742a979cfa044ed6260c4f
NOVA Developer <19577abelo@gmail.com>
2026-08-20 04:42:11 +0300

    Pure Native Android project build

--- android/app/src/main/kotlin/com/example/private_agent/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.private_agent
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- generate_icon.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image, ImageDraw, ImageFont
-
-img = Image.new('RGB', (512, 512), color='#0F172A')
-draw = ImageDraw.Draw(img)
-draw.ellipse([56, 56, 456, 456], fill='#38BDF8')
-draw.ellipse([76, 76, 436, 436], fill='#0F172A')
-
-img.save('android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png')
